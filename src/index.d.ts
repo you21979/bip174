3a6daa9b004e65d07221517333002b7414b1ee8c
yuki akiyama <you2197901@gmail.com>
2019-08-14 17:24:38 +0900

    refactor: explicit export

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,6 @@
-export { Psbt } from './lib/psbt';
+import { Psbt } from './lib/psbt';
+import * as utils from './lib/utils';
+import * as varint from './lib/converter/varint';
+export { Psbt, utils, varint };
 export * from './lib/interfaces';
 export * from './lib/typeFields';
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ function __export(m) {
   for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, '__esModule', { value: true });
-var psbt_1 = require('./lib/psbt');
+const psbt_1 = require('./lib/psbt');
 exports.Psbt = psbt_1.Psbt;
+const utils = require('./lib/utils');
+exports.utils = utils;
+const varint = require('./lib/converter/varint');
+exports.varint = varint;
 __export(require('./lib/typeFields'));
